184461d770e84a3dd8e3d85b295be46efea3388c
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-13 18:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	 if (text.trim() === '') { // Check if the string contains only spaces
+        return '';
+    }
+    for (let char of text) {
+        if (char !== ' ') { // Check for the first non-space character
+            return char;
+        }
+    }
 }
 
 // Do not change the code below
